b3639771276906f6b78a8ea781d14fbc9393d05b
adebayo <adebayoalliy@gmail.com>
2024-06-05 08:35:05 -0700

    secind

--- 0-main.c
+++ 0-main.c
@@ -11,7 +11,7 @@ int main(void)
 
     root = binary_tree_node(NULL, 98);
 
-    root->left = binary_tree_node(root, 12);
+    root->left = binary_tree_node(root, 56);
     root->left->left = binary_tree_node(root->left, 6);
     root->left->right = binary_tree_node(root->left, 16);
 
