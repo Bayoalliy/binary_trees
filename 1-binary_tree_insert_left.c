288482c6d921c8993ed92b98ae1155c067521e66
adebayo <adebayoalliy@gmail.com>
2024-06-05 09:41:45 -0700

    fixed bug

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,13 +13,15 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (!parent)
+		return (NULL);
 
 	binary_tree_t *new_node;
 
 	binary_tree_t *tmp;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node && !parent)
+	if (!new_node)
 	{
 		return (NULL);
 	}
