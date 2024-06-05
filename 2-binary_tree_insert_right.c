d17fae272bfc3a0446d2b90669965399b8f37306
adebayo <adebayoalliy@gmail.com>
2024-06-05 09:58:28 -0700

    fixed bug

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a new node to the right.
+ *
+ * @parent: pointer to the parent node.
+ *
+ * @value: value of the new node.
+ *
+ * Return: pointer to the new node(success).
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node, *tmp;
+
+	if (!parent)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+
+	tmp = parent->right;
+	parent->right = new_node;
+	new_node->right = tmp;
+	if (tmp)
+		tmp->parent = new_node;
+
+	return (new_node);
+}
