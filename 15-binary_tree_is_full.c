bc66849f715af5ddf6ead989c2547f95699cd28d
adebayo <adebayoalliy@gmail.com>
2024-06-07 09:07:24 -0700

    isfull

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - calculates the size of a tree.
+ *
+ * @tree: pointer to the root.
+ *
+ * Return: size of the node(int).
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 1;
+
+	if (!tree)
+		return (0);
+
+	if (!(tree->left) && !(tree->right))
+		return (1);
+
+	count += binary_tree_size(tree->left);
+	count += binary_tree_size(tree->right);
+	return (count);
+}
+
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full.
+ *
+ * @tree: pointer to the root.
+ *
+ * Return: (1) if full, (0) if not.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if ((binary_tree_size(tree) - 1) % 2 == 0)
+		return (1);
+
+	return (0);
+}
